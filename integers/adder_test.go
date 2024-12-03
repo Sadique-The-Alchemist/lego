265edae7c5a51f0627beea38d311886b2be9abb8
Sadique-The-Alchemist <sadique.pal@gmail.com>
2024-12-03 22:56:40 +0530

    adder

---
+++ integers/adder_test.go
@@ -0,0 +1,11 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	got := Add(2, 2)
+	want := 4
+	if got != want {
+		t.Errorf("got '%d' and expected '%d' ", got, want)
+	}
+}
