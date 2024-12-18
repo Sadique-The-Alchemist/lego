c4256ec171be7136a156c24de15910bdb1e7cef8
Sadique-The-Alchemist <sadique.pal@gmail.com>
2024-12-18 21:32:53 +0530

    perimeter

---
+++ shapes/shape_test.go
@@ -0,0 +1,37 @@
+package shape
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.00, 10.00}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{10.00, 7.00}, hasArea: 70},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.00},
+	}
+
+	for _, tt := range areaTests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g but want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
+
+}
